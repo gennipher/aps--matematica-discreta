5a06b81cb8b62654959f41f44ce9ac60f48fb92b
Jennifer Omena <jennifer.omena@m4u.com.br>
2019-08-06 18:43:54 -0300

    operações adicionadas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <OperacaoSelect v-model="operacao" />
+
+    <div>
+      <div v-if="operacao === 'permutacao'">
+        <input placeholder="Informe o valor de n" v-model="n">
+      </div>
+
+      <div v-if="operacao === 'arranjo'">
+        <input placeholder="Informe o valor de n" v-model="n">
+
+        <input placeholder="Informe o valor de p" v-model="p">
+      </div>
+
+      <div v-if="operacao === 'combinatoria'">
+        <input placeholder="Informe o valor de n1" v-model="n">
+        <input placeholder="Informe o valor de p1" v-model="p">
+      </div>
+
+    </div>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OperacaoSelect from "./components/OperacaoSelect"
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: { OperacaoSelect },
+  data () {
+    return {
+      operacao: "",
+      n: null,
+      p: null,
+    }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='scss' scoped>
+  input {
+    border: none;
+    border-bottom: solid 1px rgb(147, 147, 236);
+    margin: 0.5em;
+    padding: 1em;
+    width: 135px;
+    text-align: center
+  }
 </style>
---
+++ src/components/OperacaoSelect.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+  <label>
+    Permutação
+    <input value="permutacao"
+      type="radio"
+      name="operacao"
+      v-model="model"
+      @change="update(model)">
+  </label>
+
+  <label>
+    Arranjo
+    <input value="arranjo"
+      type="radio"
+      name="operacao"
+      v-model="model"
+      @change="update(model)">
+  </label>
+
+  <label>
+    Análise Combinatória
+    <input value="combinatoria"
+      type="radio"
+      name="operacao"
+      v-model="model"
+      @change="update">
+  </label>
+</div>
+</template>
+
+
+<script>
+export default {
+  props: ["value"],
+  data () {
+    return {
+      model: this.value
+    }
+  },
+  watch: {
+    value (val) {
+      this.model = val
+    }
+  },
+  methods: {
+    update (){
+      this.$emit('input', this.model)
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  label {
+    display: inline-block;
+    margin: 0.5em;
+    padding: 1em;
+    background-color: rgb(147, 147, 236);
+    color: white;
+    cursor: pointer;
+  }
+</style>
