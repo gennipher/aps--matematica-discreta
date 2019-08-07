31ce86b9ff19a3823457fed70d4af6776bb351c7
Jennifer Omena <jennifer.omena@m4u.com.br>
2019-08-07 19:19:24 -0300

    primeiro teste criado

--- tests/unit/calc.spec.js
+++ tests/unit/calc.spec.js
@@ -1,5 +1,11 @@
 import {calculaPermutacao, calculaArranjo, calculaCombinatoria} from "@/utils/calc.js"
 
-describe('calc.js', () => {
+test('calc.js', calculaPermutacao => {
 
+  expect(1).toBe(0);
+  expect(1).toBe(1);
+  expect(0).toBeNull();
+  expect(1).toBeUndefined(0);
+  expect(1).toBe(0);
+  expect(1).toBe(0);
 })
